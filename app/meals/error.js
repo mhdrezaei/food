0be2934d7cc3eebb2a54adf5d27980de5a8b0ca5
Mohammad Rezaei <mhdrezaei990@gmail.com>
2024-09-07 12:00:25 +0330

    handle errors

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <div className="error">
+      <h1>An Error occured!</h1>
+      <p>fetching meals has error.please try again later!</p>
+    </div>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="error">
+      <h1>Page Not Found!</h1>
+      <p>please enter valid adress!</p>
+    </div>
+  );
+}
